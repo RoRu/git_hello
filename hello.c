9284e7a225c57a66f35a31854ff9e958472f02b9
V.Yumatov <vovayumatov@gmail.com>
2014-10-07 19:44:55 +0400

    i did something with sense

--- hello.c
+++ hello.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("%s", "I don't know what's going on");
+    printf("%s", "I don't know what's going on \n");
+    system("pause");
+    return 0;
 }
